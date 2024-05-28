36944153c571e7316511449a13449de84dab9a8b
ltas1024 <83329631+ltas1024@users.noreply.github.com>
2024-05-28 02:26:36 +0000

    완성

---
+++ 기타/dollar.c
@@ -0,0 +1,21 @@
+//
+//#define _CRT_SECURE_NO_WARNINGS //����� �����ϴ� �Լ�
+//#include <stdio.h> //������� (����� �Լ��� ����ϱ� ���� �������)
+//
+//int main(void) //�����Լ�
+//{
+//	double ex_rate;
+//	double ex_dollar;
+//	int won;
+//
+//	printf("���� ȯ���� �˷��ּ���: ");
+//	scanf("%lf", &ex_rate); //ù��° ���ڸ� �Է¹޴� �Լ�
+//
+//	printf("ȯ���� ��ȭ �ݾ��� �˷��ּ���: ");
+//	scanf("%d", &won); //�ι�° ���ڸ� �Է¹޴� �Լ�
+//
+//	ex_dollar = won / ex_rate;
+//	printf("��ȭ %d ���� %lf �޷� �Դϴ� \n\n", won, ex_dollar, ex_rate);
+//
+//	return 0;	
+//}
