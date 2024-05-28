7ab101c990828e2b826a999f79e175eb044aef98
ltas1024 <83329631+ltas1024@users.noreply.github.com>
2024-05-28 02:27:23 +0000

    완성

--- 기타/dollar.c
+++ 기타/dollar.c
@@ -1,21 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS //경고를 무시하는 함수
+//#include <stdio.h> //헤더파일 (입출력 함수를 사용하기 위한 헤더파일)
 //
-//#define _CRT_SECURE_NO_WARNINGS //����� �����ϴ� �Լ�
-//#include <stdio.h> //������� (����� �Լ��� ����ϱ� ���� �������)
-//
-//int main(void) //�����Լ�
+//int main(void) //메인함수
 //{
 //	double ex_rate;
 //	double ex_dollar;
 //	int won;
 //
-//	printf("���� ȯ���� �˷��ּ���: ");
-//	scanf("%lf", &ex_rate); //ù��° ���ڸ� �Է¹޴� �Լ�
+//	printf("현재 환율을 알려주세요: ");
+//	scanf("%lf", &ex_rate); //첫번째 숫자를 입력받는 함수
 //
-//	printf("ȯ���� ��ȭ �ݾ��� �˷��ּ���: ");
-//	scanf("%d", &won); //�ι�° ���ڸ� �Է¹޴� �Լ�
+//	printf("환전할 원화 금액을 알려주세요: ");
+//	scanf("%d", &won); //두번째 숫자를 입력받는 함수
 //
 //	ex_dollar = won / ex_rate;
-//	printf("��ȭ %d ���� %lf �޷� �Դϴ� \n\n", won, ex_dollar, ex_rate);
+//	printf("원화 %d 원은 %lf 달러 입니다 \n\n", won, ex_dollar, ex_rate);
 //
 //	return 0;	
 //}
