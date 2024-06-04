2430b8c07cb7169e819347aa70b73bdeafda9f69
ltas1024 <83329631+ltas1024@users.noreply.github.com>
2024-06-04 00:18:38 +0000

    완료

---
+++ 실습10/sil10-1.c
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main() {
+//    int scores[5];
+//
+//    // 입력 부분
+//    for (int i = 0; i < 5; i++) {
+//        printf("%d번째 학생의 점수를 입력하세요 : ", i);
+//        scanf("%d", &scores[i]);
+//    }
+//
+//    // 출력 부분
+//    for (int i = 0; i < 5; i++) {
+//        printf("%d번째 학생의 점수는 %d입니다.\n", i, scores[i]);
+//    }
+//
+//    return 0;
+//}
