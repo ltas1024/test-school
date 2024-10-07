30861b89a9eca0ab102cfb8fb55b6d17b2f76e80
ltas1024 <83329631+ltas1024@users.noreply.github.com>
2024-10-07 07:11:38 +0000

    완료

--- 2학기/2/sim4.c
+++ 2학기/2/sim4.c
@@ -1,26 +1,24 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+﻿#include <stdio.h> // 헤더파일 선언
 
-#include <stdio.h>
-
-void swap(int* x, int* y);
+void swap(int x, int y); // swap 함수 선언
 
 int main(void)
 {
 	int a = 10, b = 20;
 
 	printf("a = %d, b = %d\n", a, b);
-	swap(&a, &b);
+	swap(a, b); // swap 함수 호출
 	printf("a = %d, b = %d\n", a, b);
 
 	return 0;
 
 }
 
-void swap(int* x, int* y)
+void swap(int x, int y) // swap 함수 정의
 {
-	int temp;
+	int temp; // 임시 변수 선언
 
-	temp = *x;
-	*x = *y;
-	*y = temp;
+	temp = x; // x의 값이 temp에 저장
+	x = y; // y의 값이 x에 저장
+	y = temp; // temp의 값이 y에 저장
 }
